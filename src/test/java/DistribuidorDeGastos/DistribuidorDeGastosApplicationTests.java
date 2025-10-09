d6fccd7899724ff9b8d30b61d1033da8ad8f7da4
Jonatan Alexis Rodriguez <jonatanr72@gmail.com>
2025-10-08 23:43:28 -0300

    Agregando pruebas unitarias

--- src/test/java/DistribuidorDeGastos/DistribuidorDeGastosApplicationTests.java
+++ src/test/java/DistribuidorDeGastos/DistribuidorDeGastosApplicationTests.java
@@ -1,13 +1,30 @@
 package DistribuidorDeGastos;
 
+import model.DatosDeEntradaPersona;
+import model.DatosDeSalidaPersona;
+import org.junit.Before;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.Assert.assertEquals;
 
 @SpringBootTest
 class DistribuidorDeGastosApplicationTests {
 
+	DatosDeEntradaPersona datosDeEntradaPersona;
+	DatosDeSalidaPersona datosDeSalidaPersona;
+
+	@Before
+	public void setUp() {
+		datosDeEntradaPersona = new DatosDeEntradaPersona();
+		datosDeSalidaPersona = new DatosDeSalidaPersona();
+	}
+
 	@Test
-	void contextLoads() {
+	void calcularDistribucionDeGastosEntreMiembrosDelHogarTests() {
+
+
 	}
 
 }
