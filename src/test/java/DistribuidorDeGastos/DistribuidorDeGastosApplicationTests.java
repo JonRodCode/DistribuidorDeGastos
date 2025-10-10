2ea9632b5aa2ff67228089384a985885b6c21872
Jonatan Alexis Rodriguez <jonatanr72@gmail.com>
2025-10-10 13:34:47 -0300

    Pruebas unitarias

--- src/test/java/DistribuidorDeGastos/DistribuidorDeGastosApplicationTests.java
+++ src/test/java/DistribuidorDeGastos/DistribuidorDeGastosApplicationTests.java
@@ -8,6 +8,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 import static org.junit.Assert.assertEquals;
@@ -31,6 +32,32 @@ class DistribuidorDeGastosApplicationTests {
 		DatosDeEntradaPersona datosDePersona1 = new DatosDeEntradaPersona();
 		datosDePersona1.setNombre("Pedro");
 		datosDePersona1.setGanancias(new double[] {25.0,5.2});
+		datosDePersona1.setPersonasACargo(0);
+
+		datosDePersona1.setGastosEquitativosPagados(new double[] {25.0,5.2});
+		datosDePersona1.setGastosEquitativosPendientes(new double[] {25.0,5.2});
+		datosDePersona1.setGastosIgualitariosPagados(new double[] {25.4,2.0});
+		datosDePersona1.setGastosIgualitariosPendientes(new double[]{21.2,5.0});
+
+		datosDePersona1.setGastosPersonalesDeOtros(new HashMap<String, double[]>(){{
+			put("Hola", new double[]{22.2});
+
+		}});
+		//private HashMap<String, double[]> gastosPersonalesDeOtros;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 	}
